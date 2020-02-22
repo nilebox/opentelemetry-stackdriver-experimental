222754e9e2337af503f054983efad54dd543c341
Nail Islamov <nislamov@google.com>
2020-02-22 20:51:37 +1100

    Basic skeleton for metric exporter

--- exporters/metrics/exporter.go
+++ exporters/metrics/exporter.go
@@ -2,13 +2,16 @@ package metrics
 
 import (
 	"context"
+	"errors"
+	"go.opentelemetry.io/otel/sdk/export/metric/aggregator"
+	"path"
 
 	monitoring "cloud.google.com/go/monitoring/apiv3"
 	export "go.opentelemetry.io/otel/sdk/export/metric"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
 
-	metric "google.golang.org/genproto/googleapis/api/metric"
-	monitoredres "google.golang.org/genproto/googleapis/api/monitoredres"
+	"google.golang.org/genproto/googleapis/api/metric"
+	"google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
 type Exporter struct {
@@ -21,9 +24,25 @@ var _ export.Exporter = &Exporter{}
 func (e *Exporter) Export(ctx context.Context, checkpoints export.CheckpointSet) error {
 	var timeSeries []*monitoringpb.TimeSeries
 	checkpoints.ForEach(func(record export.Record) {
+		labels, err := metricLabels(record.Labels())
+		if err != nil {
+			panic(err)
+		}
+		pt, err := point(record.Aggregator())
+		if err != nil {
+			panic(err)
+		}
 		timeSeries = append(timeSeries, &monitoringpb.TimeSeries{
-			Resource: &monitoredres.MonitoredResource{},
-			Metric:   &metric.Metric{},
+			Metric:   &metric.Metric{
+				Type: e.metricType(record.Descriptor().Name()),
+				Labels: labels,
+			},
+			Resource: &monitoredres.MonitoredResource{
+				// TODO
+			},
+			Points: []*monitoringpb.Point{
+				pt,
+			},
 		})
 	})
 	err := e.client.CreateTimeSeries(ctx, &monitoringpb.CreateTimeSeriesRequest{
@@ -34,4 +53,36 @@ func (e *Exporter) Export(ctx context.Context, checkpoints export.CheckpointSet)
 		return err
 	}
 	return nil
+}
+
+func (e *Exporter) metricType(name string) string {
+	return path.Join("custom.googleapis.com", "opentelemetry", name)
+}
+
+func metricLabels(exportLabels export.Labels) (map[string]string, error) {
+	labels := make(map[string]string)
+	for _, kv := range exportLabels.Ordered() {
+		if _, ok := labels[string(kv.Key)]; ok {
+			return nil, errors.New("duplicate label keys not allowed")
+		}
+		labels[string(kv.Key)] = kv.Value.AsString()
+	}
+	return labels, nil
+}
+
+func point(agg export.Aggregator) (*monitoringpb.Point, error) {
+	if sum, ok := agg.(aggregator.Sum); ok {
+		num, err := sum.Sum()
+		if err != nil {
+			return nil, err
+		}
+		return &monitoringpb.Point{
+			Value: &monitoringpb.TypedValue{
+				Value: &monitoringpb.TypedValue_Int64Value{
+					Int64Value: num.AsInt64(),
+				},
+			},
+		}, nil
+	}
+	return nil, errors.New("unknown aggregatot")
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/nilebox/ot-stackdriver-experimental
 go 1.13
 
 require (
-	cloud.google.com/go v0.38.0
+	cloud.google.com/go v0.53.0
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
 	go.opentelemetry.io/otel v0.2.1
-	google.golang.org/api v0.17.0 // indirect
-	google.golang.org/genproto v0.0.0-20191009194640-548a555dbc03
+	google.golang.org/genproto v0.0.0-20200212174721-66ed5ce911ce
 )
