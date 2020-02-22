835e5b348fb8ebe33dc31b7edf818af41d66885f
Nail Islamov <nilebox@gmail.com>
2020-02-22 17:42:32 +1100

    Fix types

--- exporters/metrics/exporter.go
+++ exporters/metrics/exporter.go
@@ -6,6 +6,9 @@ import (
 	monitoring "cloud.google.com/go/monitoring/apiv3"
 	export "go.opentelemetry.io/otel/sdk/export/metric"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+
+	metric "google.golang.org/genproto/googleapis/api/metric"
+	monitoredres "google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
 type Exporter struct {
@@ -19,8 +22,8 @@ func (e *Exporter) Export(ctx context.Context, checkpoints export.CheckpointSet)
 	var timeSeries []*monitoringpb.TimeSeries
 	checkpoints.ForEach(func(record export.Record) {
 		timeSeries = append(timeSeries, &monitoringpb.TimeSeries{
-			Resource: &monitoringpb.MonitoredResource{},
-			Metric:   &monitoringpb.Metric{},
+			Resource: &monitoredres.MonitoredResource{},
+			Metric:   &metric.Metric{},
 		})
 	})
 	err := e.client.CreateTimeSeries(ctx, &monitoringpb.CreateTimeSeriesRequest{
